410d72e14307ce76508af04c331060d3976cf743
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2021-10-05 16:40:05 +0200

    html-ben tábla + táblát feltöltő metódus (js)

--- film_2.js
+++ film_2.js
@@ -25,7 +25,32 @@ function hozzaad(){
 
     document.getElementById("filmNev").value = "";
     document.getElementById("filmRendezo").value = "";
-    document.getElementById("filmEv").value = 0;
+    document.getElementById("filmEv").value = "";
 }
 
 
+function btnListazas(){
+    tablaLetrehozas();
+}
+
+function tablaLetrehozas(){
+    tablaLista.style.display = "";
+
+    tablaLista.innerHTML = "";
+
+    for (let i = 0; i < filmLista.length; i++) {
+        let row = tablaListaTest.insertRow();
+
+        let td1 = row.insertCell();
+        let text1 = document.createTextNode(filmLista[i].nev);
+        td1.appendChild(text1);
+
+        let td2 = row.insertCell();
+        let text2 = document.createTextNode(filmLista[i].rendezo);
+        td2.appendChild(text2);
+
+        let td3 = row.insertCell();
+        let text3 = document.createTextNode(filmLista[i].kiadasi_ev);
+        td3.appendChild(text3);
+    }
+}
